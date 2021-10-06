fcd85bc02ad08684abad345423dcaecde71f1f6d
AdvancedEel850 <90942089+AdvancedEel850@users.noreply.github.com>
2021-10-05 23:22:13 -0600

    Update board.py

--- nim/game/board.py
+++ nim/game/board.py
@@ -1,3 +1,35 @@
 import random
+from typing import Text
 
-# TODO: Define the Board class here
+class Board:
+
+    def __init__(self) -> None:
+
+        self.piles = []
+        self._prepare()
+
+
+
+    def to_string(self):
+
+        text = "----------------------------"
+        for piles, stones in enumerate(self.piles):
+            text += (f'\n{piles}:' + ' 0 ' * stones)
+            text += "\n----------------------------"
+            return text
+
+    def apply(self,move):
+       pile = move.get_pile()
+       stone = move.get_stones()
+       self.piles[pile] = max(0, self.piles[pile] - stone)
+
+    def is_empty(self):
+        if len(self.piles) == 0:
+            return 1 == 1
+
+    def _prepare(self):
+        piles = random.randint(2,5)
+
+        for x in range(piles):
+            stones = random.randint(1,9)
+            self.piles.append(stones)
